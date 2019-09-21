bae5914e7b5046c17f6714267db055dfd61683c7
Simon William Withers Samuroff <ssamurof@coma.hpc1.cs.cmu.edu>
2019-09-21 12:58:24 -0400

    settings

--- project_settings.py
+++ project_settings.py
@@ -5,6 +5,6 @@ project settings
 import os
 
 # set directory to store Illustris particle and halo catalog data
-base_savepath = "/Volumes/G-RAID/simulations/unprocessed/Illustris/"
+base_savepath = "/home/rmandelb.proj/ssamurof/data/Illustris/"
 
 PROJECT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))+'/'
